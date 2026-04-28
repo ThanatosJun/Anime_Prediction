cdc4311b1d3752da645a67465c0fcd3292c84647
ThanatosJun <jjwang1118@gmail.com>
2026-04-28 18:17:07 +0800

    remove duplicate src/ root files (moved to src/image_branch/)

--- src/config.py
+++
@@ -1,7 +0,0 @@
-import yaml
-
-def load_config(config_path='image_process_config.yaml'):
-    with open(config_path, 'r', encoding='utf-8') as f:
-        config = yaml.safe_load(f)
-    
-    return config
